50a0fc9ddd41ad9396ec246e43413e9e3acc89ff
kira5372763 <randomout11@gmail.com>
2025-05-25 16:04:08 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Vinted Bot Automatique")
+
+st.write("Bienvenue dans ton application Vinted automatisée !")
+
+# Ici on ajoutera plus tard les fonctions : 
+# - Reposter
+# - Modifier prix
+# - Envoyer offres
+# - Voir annonces en temps réel
